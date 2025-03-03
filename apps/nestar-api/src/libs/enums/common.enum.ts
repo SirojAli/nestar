dcb7e502defa1d1df83dbaea12949379b5319dba
SirojAli <siroj.origin.profile@gmail.com>
2025-03-03 21:23:45 +0900

    feat: build project standards

---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,19 @@
+export enum Message {
+    SOMETHING_WENT_WRONG = "Something went wrong!",
+    NO_DATA_FOUND = "No Data Found!",
+    CREATE_FAILED = "Create Failed!",
+    UPDATE_FAILED = "Update Failed!",
+    REMOVE_FAILED = "Remove Failed!",
+    UPLOAD_FAILED = "Upload Failed!",
+    BAD_REQUEST = "Bad Request!",
+
+    NO_MEMBER_NICK = "No member with that member nick!",
+    BLOCKED_USER = "You have been blocked!",
+    WRONG_PASSWORD = "Wrong password, Try again!",
+    NOT_AUTHENTICATED = "You are not authenticated, Please login first!",
+    TOKEN_NOT_EXIST = "Bearer Token is not provided!",
+    ONLY_SPECIFIC_ROLES_ALLOWED = "Allowed only for members with specific role!",
+    NOT_ALLOWED_REQUEST = "Not Allowed Request!",
+    PROVIDE_ALLOWED_FORMAT = "Please provide jpg, jpeg or png images!",
+    SELF_SUBSCRIPTION_DENIED = "Self subscription denied!",
+}
