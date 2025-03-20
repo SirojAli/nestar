88ae166407c60eeb4e1a440c946eb365cdf1128a
SirojAli <siroj.origin.profile@gmail.com>
2025-03-21 04:21:48 +0900

    fix: modify view module to record visits

---
+++ apps/nestar-api/src/libs/dto/view/view.ts
@@ -0,0 +1,24 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+import { ObjectId } from "mongoose";
+import { ViewGroup } from "../../enums/view.enum";
+
+@ObjectType()
+export class View {
+  @Field(() => String)
+  _id: ObjectId;
+
+  @Field(() => ViewGroup)
+  viewGroup: ObjectId;
+
+  @Field(() => String)
+  viewRefId: ObjectId;
+
+  @Field(() => String)
+  memberId: ObjectId;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field(() => Date)
+  updatedAt: Date;
+}
