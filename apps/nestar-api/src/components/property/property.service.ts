013676cbe8a2efc6a85933732ffc0a5588efaf1a
SirojAli <siroj.origin.profile@gmail.com>
2025-04-01 20:42:49 +0900

    feat: Develop likeTargetProperty GraphQL API

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -12,6 +12,9 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
 import * as moment from 'moment';
 import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
+import { LikeService } from '../like/like.service';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
 
 @Injectable()
 export class PropertyService {
@@ -19,6 +22,7 @@ export class PropertyService {
     @InjectModel('Property') private readonly propertyModel: Model<Property>,
     private memberService: MemberService,
     private viewService: ViewService,
+    private likeService: LikeService,
 
   ) {}
 
@@ -151,6 +155,32 @@ export class PropertyService {
     if(!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
     return result[0];
   }
+
+  public async likeTargetProperty(memberId: ObjectId, likeRefId: ObjectId): Promise<Property> {
+      const target: Property = await this.propertyModel
+      .findOne({
+        _id: likeRefId,
+        propertyStatus: PropertyStatus.ACTIVE
+      }).exec();
+      if (!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+  
+      const input: LikeInput = {
+        memberId: memberId,
+        likeRefId: likeRefId,
+        likeGroup: LikeGroup.PROPERTY,
+      }
+  
+      // LIKE TOGGLE via Like Modules   (like: -1 or +1)
+      const modifier: number = await this.likeService.toggleLike(input);
+      const result = await this.propertyStatsEditor({ 
+        _id: likeRefId, 
+        targetKey: 'propertyLikes', 
+        modifier: modifier 
+      })
+  
+      if (!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+      return result;
+    }
 
   /** ADMIN **/
   public async getAllPropertiesByAdmin( input: AllPropertiesInquiry): Promise<Properties> {
